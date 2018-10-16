ba067d238a5feeaeed88cc855977d65881fd87cd
Chris Howe-Jones <chris.howejones@gmail.com>
2018-10-16 08:57:29 +0100

    Updated greeting to use time to get current day.

--- hello_app.rb
+++ hello_app.rb
@@ -1,5 +1,7 @@
 require "sinatra"
 
 get "/" do
-  "Goodbye, World!"
+  DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  dayname = DAYNAMES[Time.now.wday]
+  "Hello, World! Happy #{dayname}."
 end
