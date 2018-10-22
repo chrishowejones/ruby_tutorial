df387f8e4e01c08f6ce83675255d02c294e66e71
Chris Howe-Jones <chris.howejones@gmail.com>
2018-10-22 08:14:53 +0100

    Updated to use Date DAYNAMES

--- hello_app.rb
+++ hello_app.rb
@@ -1,7 +1,8 @@
 require "sinatra"
+require "date"
+
 
 get "/" do
-  DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  dayname = DAYNAMES[Time.now.wday]
+  dayname = Date::DAYNAMES[Time.now.wday]
   "Hello, World! Happy #{dayname}."
 end
